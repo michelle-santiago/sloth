c004bd626908a51ead4e20b89e05b552d4430268
Michelle Santiago <mdcsantiago0@gmail.com>
2022-12-05 18:18:56 +0800

    style: chat static design

--- slack-app-clone/tailwind.config.cjs
+++ slack-app-clone/tailwind.config.cjs
@@ -5,28 +5,29 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
-  themes: [
-    {
-      mytheme: {
-      "primary": "#2aa51a",
-            
-      "secondary": "#c41328",
-            
-      "accent": "#6fbbdb",
-            
-      "neutral": "#3B243D",
-            
-      "base-100": "#453A59",
-            
-      "info": "#5D9CDF",
-            
-      "success": "#22B469",
-            
-      "warning": "#F78C22",
-            
-      "error": "#ED2C56",
+  daisyui: {
+    themes: [
+      {
+        slack: {
+          "primary": "#611f69",
+                  
+          "secondary": "#c41328",
+                  
+          "accent": "#6fbbdb",
+                  
+          "neutral": "#3B243D",
+                  
+          "base-100": "#ffffff",
+                  
+          "info": "#5D9CDF",
+                  
+          "success": "#22B469",
+                  
+          "warning": "#F78C22",
+                  
+          "error": "#ED2C56",
+        },
       },
-    },
-  ],
-  
+    ],
+  },
 }
