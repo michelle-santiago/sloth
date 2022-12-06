3ed3b287074142c81e0e5e250e22607e22ac53c4
Michelle Santiago <mdcsantiago0@gmail.com>
2022-12-06 21:17:27 +0800

    feat: add channel and search modal

--- slack-app-clone/src/components/Navbar.jsx
+++ slack-app-clone/src/components/Navbar.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import SearchModal from './common/SearchModal'
+import { useNavigate } from 'react-router';
+import Dialog from './common/Dialog';
 const Navbar = () => {
+    const navigate=useNavigate();
+
+    const [dialogue, setDialogue] = useState({
+        message: "",
+        isLoading: false,
+    });
+
+    const handleDialog = (message, isLoading) => {
+        setDialogue({ message, isLoading });
+    };
+
+    const logoutConfirmation = (confirm) => {
+    if (confirm) {
+        sessionStorage.removeItem("loggedInUserAuth");
+        navigate("/login");
+    } else {
+        handleDialog("", false);
+    }
+    };
+
+    const handleLogout = () => {
+    handleDialog("Are you sure you want to sign out?", true);
+    };
+
   return (
+    <>
     <div className="flex bg-primary p-1">
         <div className="flex-1">
         </div>
         <div className="flex-1 ">
             <div className='flex flex-row gap-2'>
-            <div className="form-control w-full ">
-                <input type="text" placeholder="Search" className="w-full input input-sm input-bordered" />
+            <div  className="form-control w-full">
+                <label htmlFor="search-modal" placeholder="Search" className="w-full input input-sm input-bordered">Search</label>
             </div>
             <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-sm bg-white btn-circle avatar">
+                <label tabIndex={0} className="btn btn-sm bg-white btn-square avatar">
                     <div className="w-5 rounded-full">
                     </div>
                 </label>
@@ -23,12 +50,17 @@ const Navbar = () => {
                     </a>
                     </li>
                     <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
+                    <li><a onClick={handleLogout}>Sign Out</a></li>
                 </ul>
             </div>
             </div>
         </div>
     </div>
+    {dialogue.isLoading && (
+        <Dialog onDialog={logoutConfirmation} message={dialogue.message} />
+    )} 
+    <SearchModal/>
+    </>
   )
 }
 
