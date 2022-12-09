05e1acb3b5692a38c323c565c9ddc1621fda2449
Michelle Santiago <mdcsantiago0@gmail.com>
2022-12-09 08:35:02 +0800

    feat: retrieve channel message

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { UserContext } from "../hooks/UserContext";
 import AddChannel from "./common/AddChanel";
 import { retrieveChannels, retrieveDirectMsg } from "../api/api";
 const Sidebar = () => {
-	const { userAuthHeader, channel, setChannel, chat, setChat,id } = useContext(UserContext);
+	const { userAuthHeader, channel, setChannel, setChatType, setChannelSelected,chat, setChat,id } = useContext(UserContext);
     const user=userAuthHeader;
 	//console.log("User check side: ", user.uid);
     //console.log("channel: ",channel)
@@ -19,6 +19,25 @@ const Sidebar = () => {
         });
        
     },[])
+	const handleSelectedChannel=(channel)=>{
+		setChatType("Channel")
+		sessionStorage.setItem("chatTypeData", JSON.stringify("Channel"));
+		console.log(channel)
+		//console.log("channel id",channel.id)
+		sessionStorage.setItem("channelSelected", JSON.stringify(channel));
+		setChannelSelected(channel)
+		
+		retrieveDirectMsg(userAuthHeader,channel.id,"Channel")
+		.then((res) => {
+		  //console.log("RESPONSE MSG:",res.data)
+		  setChat(res.data)
+		  sessionStorage.setItem("chatData", JSON.stringify(res.data));
+		})
+		.catch((err) => {
+			console.log(err[0])
+		});
+
+	  }
 	return (
 		<div className="bg-primary flex-none w-64 pb-6">
 			<div className=" mb-2 px-4 flex justify-between border-b border-t border-accent pt-2 pb-2">
@@ -45,20 +64,26 @@ const Sidebar = () => {
 					</label>
 				</div>
 				<div className="h-40 overflow-y-scroll mr-1">
-					{channel.length!==0&&channel.map((channel,index) => {
-						return (
-							<div key={index} >
-								<div className="flex flex-row">
-									<div className="px-4 py-1 text-white opacity-75">
-										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-										</svg>
+					<ul>
+						{channel.length!==0&&channel.map((channel,index) => {
+							return (
+								<li key={index} className="cursor-pointer hover:bg-secondary" onClick={()=>{ handleSelectedChannel(channel)}}>
+									<div className="flex flex-row">
+										<div className="px-4 py-1 text-white opacity-75">
+											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+												<path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+											</svg>
+										</div>
+										<div className="bg-teal-dark py-1 text-white opacity-75"
+										 	>
+											{channel.name}
+											</div>
 									</div>
-									<div className="bg-teal-dark py-1 text-white opacity-75  ">{channel.name}</div>
-								</div>
-							</div>
-						)
-					})}
+								</li>
+							
+							)
+						})}
+					</ul>
 				 </div>				
 			</div>
 			<div className="mb-8">
